65f229972274b5c24b2fcc234c6f71f91568132d
Jhimi Talukder <talukderjhimi@gmail.com>
2023-10-13 21:17:40 +0600

    follow button

---
+++ index.js
@@ -0,0 +1,19 @@
+// followed buttons ----------------
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('follow-button')) {
+      var button = event.target;
+
+      if (button.innerText === "Follow") {
+        button.innerText = "Followed";
+        button.style.backgroundColor = "black";
+        button.style.color = "white";
+      } else {
+        button.innerText = "Follow";
+        button.style.backgroundColor = "";
+        button.style.color = "";
+      }
+    }
+  });
+
+
+
