eca0ac55a06321d48b73477a3983d9a06cbbf6d5
Jhimi Talukder <talukderjhimi@gmail.com>
2023-10-14 13:16:24 +0600

    almost done

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 // followed buttons ----------------
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('follow-button')) {
-      var button = event.target;
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("follow-button")) {
+    var button = event.target;
 
-      if (button.innerText === "Follow") {
-        button.innerText = "Followed";
-        button.style.backgroundColor = "black";
-        button.style.color = "white";
-      } else {
-        button.innerText = "Follow";
-        button.style.backgroundColor = "";
-        button.style.color = "";
-      }
+    if (button.innerText === "Follow") {
+      button.innerText = "Followed";
+      button.style.backgroundColor = "black";
+      button.style.color = "white";
+    } else {
+      button.innerText = "Follow";
+      button.style.backgroundColor = "";
+      button.style.color = "";
     }
-  });
-
-
-
+  }
+});
